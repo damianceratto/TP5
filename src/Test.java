63028763b492514db8ba71a370c1cef26196f277
Damian <Damian@DESKTOP-93O44JU>
2021-04-13 20:12:22 -0300

    version 1

---
+++ src/Test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Damian
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        //Arreglos.m1();
+        //Arreglos.m2();
+        //Arreglos.m3();
+        String x[][]= {{"hola", "chau", "adioses"},{"mamá"}};
+        Arreglos.m5(x);
+    
+    
+}}
